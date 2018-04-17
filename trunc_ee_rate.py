280578ba85ffb237d0dc75e9960d4c0143608b72
Zewei Song <songzewei@outlook.com>
2018-04-17 11:22:00 +0800

    switch to read in file by record

--- trunc_ee_rate.py
+++ trunc_ee_rate.py
@@ -17,32 +17,28 @@ parser.add_argument('-i', help='Input FASTQ file.')
 parser.add_argument('-o', help='Output FASTQ file.')
 parser.add_argument('-r', help='Expected Error rate, EE/bp')
 parser.add_argument('-l', help='Minimum length to keep, >=')
-parser.add_argument('-t', help='Trunk size.')
+#parser.add_argument('-t', help='Trunk size.')
 args = parser.parse_args()
 input_file = args.i
 output_file = args.o
 rate = float(args.r)
 ml = int(args.l)
-trunk = int(args.t)
+#trunk = int(args.t)
 t1 = time.clock()
-print('Reading {0} by trunk ({1}) ...'.format(input_file, trunk))
+print('Reading {0} ...'.format(input_file))
 #%% Truncate filter at maxE rate 0.01
-input_seq = io.sequence_trunk(input_file, fastx='q', trunk_size=trunk)
+input_seq = io.sequence(input_file, fastx='q')
 p_dict = qc.qual_score()
 
 i = 0
 j = 0
-c = 0
-for trunk in input_seq:
-    c += 1
-    content = []
-    for record in trunk:
-        i += 1
-        filtered = qc.trunc_ee_rate(record, p_dict, rate=rate)
-        if len(filtered[1]) >= ml:
-            content.append(filtered)
-            j += 1
-    count = io.write_seqs(content, output_file, fastx='q', mode='a')
+content = []
+for record in input_seq:
+    i += 1
+    filtered = qc.trunc_ee_rate(record, p_dict, rate=rate)
+    if len(filtered[1]) >= ml:
+        content.append(filtered)
+count = io.write_seqs(content, output_file, fastx='q', mode='a')
 print('Expected error rate = {0}, Minimum length = {1}'.format(rate, ml))
 print('Filtered {0}, kept {1}'.format(i, j))
 t2 = time.clock()
