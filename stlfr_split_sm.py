a801b58f1277109eb2fb3709d8fb608f278fd3c8
Zewei Song <songzewei@outlook.com>
2018-06-06 10:48:24 +0800

    A small memory version of stlfr_split.py

---
+++ stlfr_split_sm.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun  6 10:47:32 2018
+
+@author: Zewei Song
+@email: songzewei@genomics.cn
+"""
+#%%
+from __future__ import print_function
+from __future__ import division
+from metaSeq import io as seqIO
+
