020c3f4229a6de223b047ae81763d21829e38a64
Zewei Song <songzewei@outlook.com>
2018-06-14 12:44:00 +0800

    Create a new module for bead

---
+++ metaSeq/bead.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 11 16:59:32 2017
+
+@author: songz
+"""
+from __future__ import print_function
+from __future__ import division
+
+# Create a bead Class
+class bead(object):
+    pass
+
+
+# Remove duplicated reads from a single bead Class
+# Return a new bead Class
+def derep(bead):
+    pass
+
+
+# Remove low quality reads from a single bead Class
+# Return a new bead Class
+def maxEE(bead):
+    pass
