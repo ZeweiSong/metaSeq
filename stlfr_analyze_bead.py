0a2fff55fe3b695198124938ed028e215f964e5c
Zewei Song <songzewei@outlook.com>
2018-07-30 17:26:21 +0800

    script for analyzing bead

---
+++ stlfr_analyze_bead.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 30 16:47:24 2018
+
+@author: Zewei Song
+@email: songzewei@genomics.cn
+"""
+#%%
+from __future__ import print_function
+from __future__ import division
+import json
+from metaSeq import io as seqIO
+from metaSeq import qc as seqQC
+from metaSeq import bead
+
+# Read in the JSON bead file
+inputFile = 'mock.qc.derep.json'
+beadList = []
+with open(inputFile, 'r') as f:
+    for line in f:
+        beadList.append(bead.beadSequence(json.loads(line)))
+
+# Write the first 10 bead into FASTA file
+i = 1
+threshold = 10
+for line in beadList:
+    #outputFile = line.barcode + '.fa'
+    line.fastaWrite()
+    i += 1
+    if i > 10:
+        break
