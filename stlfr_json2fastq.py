aaeac5944b242a0f8c114208f8055eecc906cb3c
Zewei Song <songzewei@outlook.com>
2018-08-16 11:35:12 +0800

    argparse option

--- stlfr_json2fastq.py
+++ stlfr_json2fastq.py
@@ -13,19 +13,8 @@ from metaSeq import io as seqIO
 import json
 import textwrap
 
-parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter,
-                                     epilog=textwrap.dedent('''\
-                                        Convert the pair end merged FASTQ files into JSON format.
-                                        The JSON file can be read in using the Class bead_json().
-                                        By Zewei Song
-                                        Environmental Ecology Lab
-                                        Institute of Metagenomics
-                                        BGI-Research
-                                        songzewei@genomics.cn
-                                        songzewei@outlook.com
-                                        ------------------------'''))
-parser.add_argument('-a', help='FASTQ file of assembled reads.')
-parser.add_argument('-f', help='Unassembled forward reads.')
-parser.add_argument('-r', help='Unassembled reverse reads.')
-parser.add_argument('-o', help='Output bead file in JSON format.')
+parser = argparse.ArgumentParser()
+parser.add_argument('-i', help='Input beadJson format file.')
+parser.add_argument('-o', help='The output FASTQ file. Barcode will be put at -2, while /1 and /2 at -1.')
+parser.add_argument('-bp', default=-1, help='The position of barcode in beadJson. usually -1 or -2')
 args = parser.parse_args()
