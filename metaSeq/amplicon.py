58897544efdaea56e987754e962931d504f34cb3
Zewei Song <songzewei@outlook.com>
2018-10-30 09:52:19 +0800

    new amplicon module

---
+++ metaSeq/amplicon.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 30 09:51:54 2018
+
+@author: Zewei Song
+@email: songzewei@genomics.cn
+"""
+from __future__ import print_function
+from __future__ import division
