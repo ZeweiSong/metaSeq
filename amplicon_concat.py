9200cc9d91503d6852991fac3b840c57961cb45d
Zewei Song <songzewei@outlook.com>
2018-11-20 17:23:57 +0800

    Concate single profile into one

--- amplicon_concat.py
+++ amplicon_concat.py
@@ -13,25 +13,27 @@ from __future__ import print_function
 from __future__ import division
 import argparse
 from biom.table import Table
-import os
 import json
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-if', '--inputFolder', help='path/to/the/profile/folder')
+parser.add_argument('-i', '--input', type=argparse.FileType('r'), nargs='+', help='profile list separated by space')
 parser.add_argument('-biom_out', default='combined.biom', help='Output biom file name')
-#parser.add_argument('-table_out', default='combined.profile', help='Outpuit biom file name')
 args = parser.parse_args()
 
-inputFolder = args.inputFolder
 biomFile = args.biom_out
-#table_out = args.table_out
 
-fileList = os.listdir(inputFolder)
-biomList = [i for i in fileList if i[-5:] == 'biom']
-print('Found {0} biom profiles under {1}.'.format(len(biomList), inputFolder))
-with open(inputFolder + '/' + biomList[0], 'r') as f:
-    biomProfile = Table.from_json(json.load(f))
+biomList = []
+for f in args.input:
+    biomList.append(f)
+if len(biomList) <= 1:
+    print('One or less profile found, are you sure?')
+else:
+    print('Found {0} biom profiles under.'.format(len(biomList)))
+    biomProfile = Table.from_json(json.load(biomList[0]))
 
-for item in biomList[1:]:
-    with open(item, 'r') as f:
-        biomProfile = biomProfile.concat([Table.from_json(f)])
+    for f in biomList[1:]:
+        biomProfile = biomProfile.concat([Table.from_json(json.load(f))])
+
+    with open(biomFile, 'w') as f:
+        biomProfile.to_json('Generated_almighty_metaSeq', f)
+    print('Concatenated {0} profiles into {1}.'.format(len(biomList), biomFile))
