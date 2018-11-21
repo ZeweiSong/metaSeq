3911e4e65a6827801928703c5fd5c235defdd859
Zewei Song <songzewei@outlook.com>
2018-11-21 15:53:50 +0800

    Place holder for amplicon split function

--- amplicon_split.py
+++ amplicon_split.py
@@ -12,3 +12,14 @@ from __future__ import print_function
 from __future__ import division
 import argparse
 from metaSeq import io as seqIO
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-r1', help='R1 reads')
+parser.add_argument('-r2', help='R2 reads if needed')
+parser.add_argument('-b', '--barcode', help='Barcode mapping file with sample info')
+parser.add_argument('-o', '--output', help='Output folder for splitted files')
+args = parser.parse_args()
+r1File = args.r1
+r1f = seqIO.sequence(r1File, fastx='q', gz=True)
+if args.r2:
+    r2File = args.r2
