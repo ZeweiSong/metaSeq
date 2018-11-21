25bee98b59feed150401e20b7026e6c6ad49bcc6
Zewei Song <songzewei@outlook.com>
2018-11-21 11:43:28 +0800

    place holder for split function

--- amplicon_split.py
+++ amplicon_split.py
@@ -3,8 +3,12 @@
 """
 Created on Wed Nov 21 11:42:28 2018
 
+A place holder for split function.
+
 @author: Zewei Song
 @email: songzewei@genomics.cn
 """
 from __future__ import print_function
 from __future__ import division
+import argparse
+from metaSeq import io as seqIO
