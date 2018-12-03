60848d74733d67fb8362e523084ee551a8854811
Zewei Song <songzewei@outlook.com>
2018-12-03 13:37:49 +0800

    add several options

--- amplicon_winnerTakeAll.py
+++ amplicon_winnerTakeAll.py
@@ -14,25 +14,29 @@ from biom.table import Table
 import numpy as np
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-i', help='File names of the two alignment output.')
+parser.add_argument('-i', nargs='+', help='File names of the two alignment output.')
 parser.add_argument('-t', '--tab_out', help='File name of tab-delimited format output.')
 parser.add_argument('-b', '--biom_out', help='File name of biom format output.')
-parser.add_argument('-sn', default= 'SampleX', help="Sample name if specified.")
+parser.add_argument('-sn', default='SampleX', help="Sample name if specified.")
+parser.add_argument('-suppress_progress', action='store_true', help='Indicators for suppressing progress.')
 args = parser.parse_args()
 
-alnString = args.i
-#alnString = 'even1.forward.unite_ee.burstFORAGE97.b6,even1.reverse.unite_ee.burstFORAGE97.b6' # Provide alignments files on different targets
-alnString = alnString.strip('\n').split(',')
-TargetNumber = len(alnString) # The number of targets used (normally this is 2, e.g. ITS1 forward & ITS1 reverse)
+alnString = []
+for item in args.i:
+    alnString.append(item)
+targetNumber = len(alnString) # The number of targets used (normally this is 2, e.g. ITS1 forward & ITS1 reverse)
 sampleName = args.sn
 tabFile = args.tab_out
 biomFile = args.biom_out
+progress = True
+if args.suppress_progress:
+    progress = False
 
-print('Found {0} targets from your input.'.format(len(alnString)))
+print('Found {0} targets from your input.'.format(targetNumber))
 print('Normalizing alignments ...')
 alnNormalized = amplicon.initAlignment(alnString)
 print('Only one winner survives each round ...')
-wtaProfile = amplicon.winnerTakeAll(alnNormalized)
+wtaProfile = amplicon.winnerTakeAll(alnNormalized, progress=progress)
 print('Winner take all profile found! {0} references survived.'.format(len(wtaProfile)))
 profile = list(wtaProfile.items())
 profile.sort(key=lambda x:x[1], reverse=True)
