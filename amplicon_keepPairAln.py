031181ebf7021fbe665a189dac36cf734f6b0d7c
Zewei Song <songzewei@outlook.com>
2018-12-12 08:54:35 +0800

    fix the pair bug

--- amplicon_keepPairAln.py
+++ amplicon_keepPairAln.py
@@ -19,18 +19,16 @@ outputAln = args.output
 
 # Build the query dictionary
 # Two alignments are combined into one
+#%%
+#inputAln = ['501.r1fw.b6', '501.r2rv.b6']
 queryDict = {}
 with open(inputAln[0], 'r') as f:
     for line in f:
         line = line.strip('\n').split('\t')
         line[0] = line[0].split('/')[0]
-        try:
-            queryDict[line[0]]['r1'].append(line[1])
-        except KeyError:
-            try:
-                queryDict[line[0]]['r1'] = [line[1]]
-            except KeyError:
-                queryDict[line[0]] = {}
+        queryDict[line[0]] = {'r1':[], 'r2':[]}
+        queryDict[line[0]]['r1'].append(line[1])
+
 with open(inputAln[1], 'r') as f:
     for line in f:
         line = line.strip('\n').split('\t')
@@ -38,10 +36,9 @@ with open(inputAln[1], 'r') as f:
         try:
             queryDict[line[0]]['r2'].append(line[1])
         except KeyError:
-            try:
-                queryDict[line[0]]['r2'] = [line[1]]
-            except KeyError:
-                queryDict[line[0]] = {}
+            queryDict[line[0]] = {'r1':[], 'r2':[]}
+            queryDict[line[0]]['r2'].append(line[1])
+#%%
 aln = {}
 for query, alignments in queryDict.items():
     try:
@@ -50,7 +47,7 @@ for query, alignments in queryDict.items():
             aln[query] = overlap
     except KeyError:
         pass
-
+#%%
 with open(outputAln, 'w') as f:
     for query, alignments in aln.items():
         for item in alignments:
--- untitled0.py
+++ untitled0.py
@@ -1,10 +1,33 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
-Created on Wed Nov 21 11:42:28 2018
+Created on Tue Dec 11 15:27:36 2018
+
+Change the unite sequence file label to SH only, and save the taxa in a single file
 
 @author: Zewei Song
 @email: songzewei@genomics.cn
 """
+#%%
 from __future__ import print_function
 from __future__ import division
+from metaSeq import io as seqIO
+
+handle = seqIO.sequence('ee_its_database.fasta', fastx='a')
+unite = []
+for item in handle:
+    unite.append(item)
+
+fasta = []
+taxa = []
+for item in unite:
+    label = item[0].split('|')
+    newLabel = label[0] + '|' + label[2]
+    fasta.append((newLabel, item[1]))
+    taxa.append((newLabel, label[4]))
+
+seqIO.write_seqs(fasta, 'ee_its_sequences.fa', fastx='a')
+with open('ee_its_taxonomy.txt', 'w') as f:
+    for line in taxa:
+        line = '\t'.join(line)
+        f.write('{0}\n'.format(line))
