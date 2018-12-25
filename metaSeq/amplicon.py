3e12d6d69f727ac44748f41563d6beb1d1365795
Zewei Song <songzewei@outlook.com>
2018-12-25 17:45:12 +0800

    Fix wrong count in Less greedy method

--- metaSeq/amplicon.py
+++ metaSeq/amplicon.py
@@ -81,9 +81,9 @@ def effectiveCount(countString):
         ec = ave - stdev
         if ec < 0:
             ec = 0.0
-        return ec
+        return (ec, ave, stdev)
     else:
-        return countString[0]
+        return (countString[0], countString[0], 0)
 
 # Return the new Graph after market competition
 def competition(graph, greedy=True):
@@ -94,14 +94,15 @@ def competition(graph, greedy=True):
     losers = [] # List for losers (ref with abundance < 1)
     for ref in references.keys():
         effScore = effectiveCount(graph.nodes[ref]['abundance'])
-        if effScore >= 1.0:
+        if effScore[0] >= 1.0:
             refSurvivors[ref] = effScore
         else:
             losers.append(ref)
 
     # Get the winner (ref with largest EF)
-    winner = sorted([i for i in refSurvivors.items()], key=lambda x:x[1], reverse=True)[0]
+    winner = sorted([i for i in refSurvivors.items()], key=lambda x:x[1][0], reverse=True)[0]
     winner = list(winner)
+    winner[1] = list(winner[1])
 
     # Remove winner queires
         # Has two methods: Greedy and Less greedy
@@ -117,27 +118,28 @@ def competition(graph, greedy=True):
         ref = winner[0]
         # allocate queries to corresponding target by their number
         queries = graph.neighbors(ref)
-        pool = [[]] * graph.graph['targetNumber']
+        pool = {}
+        for i in range(graph.graph['targetNumber']):
+            pool[i] = []
         for q in list(queries):
             pool[graph.nodes[q]['attribute']].append(q)
-        minLength = min([len(i) for i in pool]) # Get the min size of the target
+        minLength = min([len(i) for i in pool.values()]) # Get the min size of the target
         querySurvived = []
-        for target in pool:
+        for target in pool.values():
             if len(target) > minLength:
                 random.shuffle(target) # shuffle the query list
-                target.sort(key=lambda x:graph.degree(x)[0]) # Sort query by their degree
+                target.sort(key=lambda x:graph.degree(x)) # Sort query by their degree
                 querySurvived += target[minLength:]
         queries = [x for x in list(queries) if x not in querySurvived]
         graph.remove_nodes_from(list(queries)) # Remove all winner's query nodes
         graph.remove_node(ref) # Remove the winner node
         del graph.graph['ref'][ref]
-        winner[1] = minLength
+        winner[1][0] = minLength
         # Update the abundance string for the rest of references
         graph = addAbundance(graph, graph.graph['targetNumber'])
 
     # Remove all losers from the graph
     for ref in losers:
-
         if greedy:
             queries = graph.neighbors(ref)
             graph.remove_nodes_from(list(queries))
@@ -153,7 +155,7 @@ def competition(graph, greedy=True):
                 pass
 
     # Update the profile with the latest winner
-    graph.graph['profile'][winner[0]] = winner[1]
+    graph.graph['profile'][winner[0]] = winner[1][0]
     return graph
 #%%
 # Define the winner take all function
@@ -166,59 +168,3 @@ def winnerTakeAll(aln, progress=False, greedy=False):
         if progress:
             print('\t{0} references left, {1} are in the profile,'.format(len(G.graph['ref']), len(G.graph['profile'])))
     return G.graph['profile']
-
-#%% Old logic for winner take all, save just for case
-'''
-# Pick the winner from the graph (with abundance tuple)
-# TODO after the sorting, remove the zero abundance tail to speed up
-def pickWinner(graph):
-    # Iterate over all reference nodes:
-    abundanceDict = {}
-    for ref in graph.graph['ref'].keys():
-        abundanceDict[ref] = effectiveCount(graph.nodes[ref]['abundance'])
-    candidates = [i for i in abundanceDict.items() if i[1] >= 1] # Keep references with EF >= 1
-    candidates.sort(key=lambda x:x[1], reverse = True)
-    #print(candidates)
-    return candidates
-#%%
-# Remove winner from the graph, remove all its neighbors
-# Winner is the name of the reference
-# TODO Also need to remove all references with EF < 1
-def removeWinner(graph, candidates):
-    # STEP1. Remove the winner
-    removeQueries = list(graph.neighbors(candidates[0][0]))
-    graph.remove_nodes_from(removeQueries)
-    graph.remove_node(candidates[0][0])
-    del graph.graph['ref'][candidates[0][0]]
-
-    # STEP2. Remove the losers (references not in cadidates list)
-    losers = [i for i in graph.graph['ref'].items() if i[0] not in [j[0] for j in candidates]] #TODO need to fix this one
-    print(losers)
-    for item in losers:
-        removeQueries = list(graph.neighbors(item[0]))
-        graph.remove_nodes_from(removeQueries)
-        graph.remove_node(item[0])
-        del graph.graph['ref'][item[0]]
-    return graph
-
-# Return the winner take all profile
-# TODO need to change according to pickWinner()
-def winnerTakeAll(aln):
-    G = initGraph(aln)
-    targetNumber = len(aln)
-    G = addAbundance(G, targetNumber)
-    while not nx.is_empty(G):
-        candidates = pickWinner(G)
-        if candidates[0][1] <= 0: # Stop if the winner is ZERO (or maybe 1?)
-            break
-        else:
-            G.graph['profile'][candidates[0][0]] = candidates[0][1]
-            print(candidates[0])
-            G = removeWinner(G, candidates)
-            G = addAbundance(G, targetNumber)
-    profile = {}
-    for ref in G.graph['profile'].keys():
-        if G.graph['profile'][ref]: # This ref is not empty (broke)
-            profile[ref] = G.graph['profile'][ref]
-    return profile
-'''
