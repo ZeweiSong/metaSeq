e00038c56d65e7391ed7b89d1bc13f3fdf5f0884
Fangchao <fangchao@genomics.cn>
2020-04-02 11:03:32 +0800

    custemized adaptor seqs for RCACLIP

--- src/RCAasm.sh
+++ src/RCAasm.sh
@@ -22,7 +22,9 @@ cluster=$4
 tag=$5
 mem=$6
 cpu=$7
-force=$8
+config=$8
+force=$9
+
 
 clusterFmt=`printf "%08.0f" $cluster`
 if [[ $level -gt 0 ]];
@@ -103,15 +105,18 @@ else
 	$cmd
   elif [ $mode == "megahit" ]; then
     echo "[BC] megahit selected."
-    megahit --k-min 21 --k-step 22 --k-max 121  -m $mem -t $cpu \
+#    megahit --k-min 21 --k-step 22 --k-max 121  -m $mem -t $cpu \
+    megahit --k-step 6 --prune-level 0 --min-count 1 -m $mem -t $cpu \
     -1 $samDir/$ASB/$subDir/sort.1.fq -2 $samDir/$ASB/$subDir/sort.2.fq \
     -o $samDir/$ASB/$subDir/$rdir &> $samDir/$ASB/$subDir/megahit.log
   fi
 fi
 
+#read primers from config
+while read line;do eval "$line"; done < $config
 echo
 echo "Run RCACLIP"
-cmd="metabbq RCACLIPS -i $scaf -o $clip -v 2> ${clip/fa/log}"
+cmd="metabbq RCACLIPS -i $scaf -o $clip -fwd $FWD -rev $REV -a $Ad -v 2> ${clip/fa/log}"
 echo "exec: $cmd"
 $cmd
 echo "DONE"
